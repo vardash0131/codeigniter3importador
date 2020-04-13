5bd988d65f4ade8dcb98034eaf5e4cc678aad2ae
uliseses1325 <ulises.caballero@bambu.ninja>
2020-04-13 14:01:56 -0500

    inicializando proyecto

---
+++ application/controllers/Importador.php
@@ -0,0 +1,151 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Importador extends CI_Controller {
+
+public function __construct()
+{
+    parent::__construct();
+    $this->load->model('importador_model','modelo');
+}
+    public function index()
+    {
+       
+        $this->read_file();
+    }
+     /**
+     * funcion publica la cual ejecutara el cron
+     */
+    public function read_file()
+    {
+        $this->get_server_file();
+    }
+    /**
+     * { function_description }
+     *
+     * @param      array   $array     array de datos
+     * @param      <type>  $database  base de datos
+     * @param      <type>  $table     tabla
+     */
+    private function process_file_directo(array $array,$database,$table,$file)
+    {
+        return $this->importador->onInsert($array,$database,$table,$file);
+    }
+    /**
+     * funcion procesa archivos de tipo txt con formato campo1|campo2|campo3 y retorna array asociativo
+     *
+     * @param      <File>  $file   el archivo
+     *
+     * @return     <Array>  Array Asociativo
+     */
+    private function file_to_array($file) {
+
+            $csv= array_map(function($v){return str_getcsv($v, ",");}, file($file));
+            // echo memory_get_usage()."<br>";
+            array_walk($csv, function(&$a) use ($csv) {
+              $a = array_combine($csv[0], $a);
+            });
+            // echo memory_get_usage();
+            array_shift($csv);
+
+            if (empty($csv)) {
+                $path_parts=pathinfo($file);
+                rename(
+                    $file,
+                    APPPATH."files/no_proccess/".$path_parts['basename']
+                );
+                exit();
+            }
+            return $csv;
+    }
+    /**
+     * funcion que obtiene todos los archivos del servidor
+     */
+    private function get_server_file()
+    {
+        $list = glob(FCPATH."files/to_import/*.csv");
+
+        if (!empty($list)) {
+
+            foreach ($list as $key => $value) {
+
+                $path_parts = pathinfo($value);
+                $nombre = $path_parts['filename'];
+                $nombre=str_replace("_", " ", $nombre);
+                $nombre=str_replace("-", " ", $nombre);
+                $nombre=str_replace(".", " ", $nombre);
+                $this->name_cases($nombre,$value);
+                sleep(4);
+            }
+        }
+    }
+
+    /**
+     * funcion encargada de validar que el archivo tenga el nombre
+     * correcto para poder ser cargado en caso de no coincidencia
+     * se mueve de carpeta
+     *
+     * @param      <type>  $name   nombre del archivo
+     * @param      <type>  $file   el archivo
+     */
+    private function name_cases($name,$file)
+    {
+        $bases = ['internet','clickbus','celular'];
+        foreach ($bases as $obligatorio) {
+            $palabra = $obligatorio;
+            $existe = strpos(strtolower($name), $palabra);
+
+            if ($existe !== false) {
+                    $estatus=$this->importar(
+                        $this->file_to_array($file),
+                        $name,
+                        $palabra
+                    );
+                    $path_parts=pathinfo($file);
+                    if($estatus){
+                       rename(
+                            $file,
+                            FCPATH."files/imported/".$path_parts['basename']
+                        );
+                       echo "archivo ".$path_parts['basename']." fue cargado con exito";
+                       echo "<br>";
+                    }
+                    else {
+                          rename(
+                            $file,
+                            FCPATH."files/no_proccess/".$path_parts['basename']
+                        );
+                        echo "ocurrio un error en el archivo ".$path_parts['basename'];
+                        echo "<br>";
+                    }
+
+            }
+        }
+    }
+    /**
+     * function carga de archivo
+     *
+     * @param      array   $array  The array
+     * @param      <type>  $file   el archivo
+     * @param      <type>  $tipo   The tipo
+     *
+     * @return     <type>  ( description_of_the_return_value )
+     */
+    private function importar(array $array,$file,$tipo)
+    {
+
+         if ( isset($array[0]['Fecha Corrida']) ) {
+            $fecha = $array[0]['Fecha Corrida'];
+            $fecha = $date = str_replace('/', '-', $fecha);
+
+            $fecha = strtotime($fecha);
+            $fecha  = date('m-Y',$fecha);
+            $fecha = explode("-", $fecha);
+
+            $tabla = $this->modelo->tableinicial($fecha[0],$fecha[1]);
+            return $this->modelo->importar($array,$file,$tipo,$tabla);
+         }
+
+    }
+
+}
---
+++ application/models/Importador_model.php
@@ -0,0 +1,193 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Importador_model extends CI_Model{
+
+    function __construct()
+    {
+        parent::__construct();
+    }
+    private function coct($table, $fields)
+    {
+        if (!$this->db->table_exists($table)) {
+            $this->load->dbforge();
+            $this->dbforge->add_key('id', TRUE);
+            $this->dbforge->add_field($fields);
+            if($this->dbforge->create_table($table)) {
+                return $table;
+            } else {
+                return false;
+            }
+        } else {
+            return $table;
+        }
+    }
+    public function tableinicial($mes,$anio)
+    {
+
+        $table = 'transacciones_'.$mes."_".$anio;
+        $fields = array(
+             'id' => array(
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => TRUE,
+                'auto_increment' => TRUE
+            ),
+            'origen_del_viaje' => array(
+                'type' =>'VARCHAR',
+                'constraint' => '250',
+                'unsigned' => true,
+            ),
+            'destino_del_viaje' => array(
+                'type' =>'VARCHAR',
+                'constraint' => '250',
+                'unsigned' => true,
+            ),
+            'numero_de_asiento' => array(
+                'type' => 'VARCHAR',
+                'constraint' => 250,
+            ),
+            'tipo_de_boleto' => array(
+                'type' => 'VARCHAR',
+                'constraint' => 250,
+            ),
+            'numero_de_folio' => array(
+                'type' => 'VARCHAR',
+                'constraint' => 250,
+            ),
+            'concepto' => array(
+                'type' => 'VARCHAR',
+                'constraint' => 250,
+            ),
+            'tipo_de_forma_de_pago' => array(
+                'type' => 'VARCHAR',
+                'constraint' => 250,
+            ),
+            'importe_sin_iva' => array(
+                'type' => 'VARCHAR',
+                'constraint' => 250,
+            ),
+            'iva' => array(
+                'type' => 'VARCHAR',
+                'constraint' => 250,
+            ),
+            'importe_total' => array(
+                'type' => 'VARCHAR',
+                'constraint' => 250,
+            ),
+            'fecha_venta' => array(
+                'type' => 'VARCHAR',
+                'constraint' => 250,
+            ),
+            'hora_venta' => array(
+                'type' => 'VARCHAR',
+                'constraint' => 250,
+            ),
+            'numero_de_operacion' => array(
+                'type' => 'VARCHAR',
+                'constraint' => 250,
+            ),
+            'clase_de_servicio' => array(
+                'type' => 'VARCHAR',
+                'constraint' => 250,
+            ),
+            'nombre_asesor' => array(
+                'type' => 'VARCHAR',
+                'constraint' => 250,
+            ),
+            'descripcion_marca' => array(
+                'type' => 'VARCHAR',
+                'constraint' => 250,
+            ),
+            'numero_de_corrida' => array(
+                'type' => 'VARCHAR',
+                'constraint' => 250,
+            ),
+            'fecha_corrida' => array(
+                'type' => 'VARCHAR',
+                'constraint' => 250,
+            ),
+            'hora_corrida' => array(
+                'type' => 'VARCHAR',
+                'constraint' => 250,
+            ),
+            'mes_de_la_corrida' => array(
+                'type' => 'VARCHAR',
+                'constraint' => 250,
+            ),
+            'tipo_de_corrida' => array(
+                'type' => 'VARCHAR',
+                'constraint' => 250,
+            ),
+            'hora_de_emision' => array(
+                'type' => 'VARCHAR',
+                'constraint' => 250,
+            ),
+            'correo_usuario' => array(
+                'type' => 'VARCHAR',
+                'constraint' => 250,
+            ),
+            'tipo_de_carga' => array(
+                'type' => 'VARCHAR',
+                'constraint' => 50,
+            ),
+            'created_at' => array(
+                'type' => 'DATETIME',
+            ),
+             'updated_at' => array(
+                'type' => 'DATETIME',
+            ),
+        );
+        $this->coct($table, $fields);
+        return $table;
+    }
+    public function importar($array,$file,$tipo,$tabla)
+    {
+        $sale_ids_chunk = array_chunk($array, 1000);
+        foreach($sale_ids_chunk as $sale_ids)
+        {
+            $insert=$this->format_db($array,$tipo);
+            $this->db->insert_batch($tabla, $insert);
+        }
+
+        return TRUE;
+    }
+    private function format_db($array,$tipo)
+    {
+        try {
+             return array_map(function($array)  use($tipo){
+             $arrayreturn=array(
+                'tipo_de_carga' => $tipo,
+                'origen_del_viaje' => $array['Origen del Viaje'],
+                'destino_del_viaje' => $array['Destino del Viaje'],
+                'numero_de_asiento' => $array['Número de Asiento'],
+                'tipo_de_boleto' => $array['Tipo de Boleto'],
+                'numero_de_folio' => $array['Número de Folio de Boleto'],
+                'concepto' => $array['Concepto'],
+                'tipo_de_forma_de_pago' => $array['Tipo de Forma de Pago'],
+                'importe_sin_iva' => $array['Importe sin IVA'],
+                'iva' => $array['IVA'],
+                'importe_total' => $array['Importe Total'],
+                'fecha_venta' => $array['Fecha Venta'],
+                'hora_venta' => $array['Hora Venta'],
+                'numero_de_operacion' => $array['Número de Operación'],
+                'clase_de_servicio' => $array['Clase de Servicio'],
+                'nombre_asesor' => $array['Nombre Asesor'],
+                'descripcion_marca' => $array['Descripción Marca'],
+                'numero_de_corrida' => $array['Número de Corrida'],
+                'fecha_corrida' => $array['Fecha Corrida'],
+                'hora_corrida' => $array['Hora Corrida'],
+                'mes_de_la_corrida' => $array['Mes de la corrida'],
+                'tipo_de_corrida' => $array['Tipo de Corrida'],
+                'hora_de_emision' => $array['Hora de Emisión del Reporte'],
+                'correo_usuario' => $array['Correo Usuario'],
+                'created_at'=> date('Y-m-d H:i:s'),
+                'updated_at'=>date('Y-m-d H:i:s'),
+            );
+                return $arrayreturn;
+            }, $array);
+        } catch (Exception $e) {
+            echo "un archivo no tiene el formato esperado";
+            exit();
+        }
+    }
+}
